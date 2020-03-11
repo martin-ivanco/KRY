5fb79902b13e3d2d84621af8489ff643665d254b
photohunter9 <ivancom.fr@gmail.com>
2020-03-11 14:22:04 +0100

    Project 1 Done

--- otp.py
+++ otp.py
@@ -1,5 +1,8 @@
 import argparse
 import base64
+import json
+import os
+from progressbar import progressbar
 
 class Message:
     def __init__(self, values):
@@ -44,91 +47,156 @@ class Message:
         return iter(self.values)
 
 class Crib:
-    def __init__(self, crib_word, messages):
-        self.word = crib_word
-        self.message_matches = {}
-        for i, m in enumerate(messages):
-            self.message_matches[i] = [0] * len(m)
-        self.search(messages)
+    def __init__(self, crib_word):
+        self.word = Message(crib_word)
 
     def search(self, messages):
+        self.message_matches = {}
+        for i, m in enumerate(messages):
+            self.message_matches[i] = [1] * len(m)
+
         for i1, m1 in enumerate(messages):
             for i2, m2 in enumerate(messages):
-                if i2 <= i1:
-                    continue
-                self.match(i1, m1, i2, m2)
+                if i2 > i1:
+                    self.match(i1, m1, i2, m2)
 
     def match(self, i1, m1, i2, m2):
         xored = m1 + m2
         for j in range(len(xored) - len(self.word)):
-            other = Message(xored[j:]) + self.word
-            if str(other).lower() in OTPCracker.crib_words:
-                self.message_matches[i1][j] += 1
-                self.message_matches[i2][j] += 1
-
+            if self.message_matches[i1][j] or self.message_matches[i2][j]:
+                other = Message(xored[j:]) + self.word
+                if all(v in OTPCracker.printable for v in other.values):
+                    if self.message_matches[i1][j] > 0:
+                        self.message_matches[i1][j] += 1
+                    if self.message_matches[i2][j] > 0:
+                        self.message_matches[i2][j] += 1
+                else:
+                    self.message_matches[i1][j] = 0
+                    self.message_matches[i2][j] = 0
 
 class OTPCracker:
-    # crib_words = ["pro", "ova", "ost", "pri", "sta", "pre", "ter", "eni", "pod", "kte", "pra",
-    #               "eho", "sti", "red", "kon", "nos", "ick", "sou", "ist", "edn", "ske", "odn",
-    #               "tel", "ani", "ent", "str", "ove", "nov", "pol", "spo", "vat", "nim", "jak",
-    #               "val", "dni", "sto", "tak", "lov"]
-    crib_words = ["st", "pr", "ne", "ni", "po", "ov", "ro", "en", "na", "je", "te", "le", "ko",
+    printable = [32, 44, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70, 71,
+                 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97,
+                 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113,
+                 114, 115, 116, 117, 118, 119, 120, 121, 122]
+    trigrams = ["pro", "ova", "ost", "pri", "sta", "pre", "ter", "eni", "pod", "kte", "pra",
+                  "eho", "sti", "red", "kon", "nos", "ick", "sou", "ist", "edn", "ske", "odn",
+                  "tel", "ani", "ent", "str", "ove", "nov", "pol", "spo", "vat", "nim", "jak",
+                  "val", "dni", "sto", "tak", "lov"]
+    bigrams = ["st", "pr", "ne", "ni", "po", "ov", "ro", "en", "na", "je", "te", "le", "ko",
                   "od", "ra", "to", "ou", "no", "la", "li", "ho", "do", "os", "se", "ta", "al",
                   "ed", "an", "ce", "va", "at", "re", "er", "ti", "em", "in", "sk", "lo"]
 
-    def __init__(self, messages):
-        self.messages = messages
-        self.key = {}
+    def __init__(self, crib_words):
+        self.cribs = [Crib(cw) for cw in crib_words]
+        self.keys = []
+        self.maxlen = 0
     
-    def crack(self):
-        cribs = []
-        for cw in self.crib_words:
-            cribs.append(Crib(Message(cw), self.messages))
-        
-        for c in cribs:
-            print("-----------------------------------------")
-            print(str(c.word))
-            print("-----------------------------------------")
+    def crack(self, messages):
+        # check the last key against messages
+        if self.keys:
+            last_key = self.keys[len(self.keys) - 1]
+            for m in messages:
+                decrypted = m + self.key2msg(last_key)
+                print(str(decrypted))
+                for i, v in enumerate(decrypted.values):
+                    if v not in self.printable and i in last_key:
+                        last_key.pop(i)
+
+        # search for cribs in messages
+        self.keys.append({})
+        last_key = self.keys[len(self.keys) - 1]
+        for c in progressbar(self.cribs):
+            c.search(messages)
             for idx, match in c.message_matches.items():
                 for i, v in enumerate(match):
-                    if v < 2:
-                        continue
-                    subkey = Message(self.messages[idx][i:]) + c.word
-                    self.add_subkey(subkey, i)
-                    print("Message: {}; Position: {}; Subkey: {}".format(idx, i, subkey.values))
-                    for m in self.messages:
-                        print(str(Message(m[i:]) + subkey))
-            print("-----------------------------------------")
-        print(self.key)
+                    if v >= len(messages):
+                        subkey = Message(messages[idx][i:]) + c.word
+                        self.add_subkey(last_key, subkey, i)
 
-        xor_key = [0] * max([len(m) for m in self.messages])
-        for i, vs in self.key.items():
-            if len(vs) < 2:
-                xor_key[i] = vs.pop()
-        
-        return xor_key
+        # remove conflicts from key
+        print("Key: {}".format(last_key))
+        # with open("test1.json", "w") as f:
+        #     for i, vs in last_key.items():
+        #         last_key[i] = list(vs)
+        #     json.dump(last_key, f)
+        self.maxlen = max(self.maxlen, max([len(m) for m in messages]))
+        for i in list(last_key):
+            if len(last_key[i]) > 1:
+                last_key.pop(i)
     
-    def add_subkey(self, subkey, idx):
+    def add_subkey(self, key, subkey, idx):
         for i, k in enumerate(subkey):
-            if idx + i in self.key:
-                self.key[idx + i].add(k)
+            if idx + i in key:
+                key[idx + i].add(k)
             else:
-                self.key[idx + i] = set([k])
+                key[idx + i] = set([k])
+
+    def key2msg(self, key):
+        values = [-1] * self.maxlen
+        for i, vs in key.items():
+            for v in vs:
+                break
+            values[i] = v
+        
+        return Message(values)
+
+    def save_keys(self, filename):
+        j = []
+        for k in self.keys:
+            j.append(self.key2msg(k).values)
+        with open(filename, "w") as file:
+            json.dump(j, file)
+
+    def merge_keys(self):
+        key = {}
+        for k in self.keys:
+            for i, vs in k.items():
+                for v in vs:
+                    break
+                if i in key:
+                    key[i].add(v)
+                else:
+                    key[i] = set([v])
+
+        for i in list(key):
+            if len(key[i]) > 1:
+                key.pop(i)
+
+        return self.key2msg(key)
 
 def main():
+    # parse arguments
     parser = argparse.ArgumentParser(description="Weakend One-Time Pad Algorithm Cracker")
-    parser.add_argument("messages", type = str, metavar = "<filename>",
-        help = "path to file with encrypted messages encoded in base64 and separated by new line")
+    parser.add_argument("msg_folder", type = str, metavar = "<filename>",
+        help = "path to folder with files with encrypted messages encoded in base64 and separated "
+               "by new line")
     args = parser.parse_args()
+    
+    # load crib words dictionary
+    with open("words.json") as file:
+        words = json.load(file)
 
-    with open(args.messages) as file:
-        lines = file.readlines()
-    lines = [list(base64.b64decode(l.strip())) for l in lines]
-    cracker = OTPCracker([Message(l) for l in lines])
-    key = cracker.crack()
-    with open("key.txt", "wb") as file:
-        file.write(base64.b64encode(bytes(key)))
-        file.write(bytes("\n".encode()))
+    cracker = OTPCracker(words)
+
+    # go through message files
+    for f in os.listdir(args.msg_folder):
+        if not f.endswith(".txt"):
+            continue
+        with open(os.path.join(args.msg_folder, f)) as file:
+            lines = file.readlines()
+        lines = [list(base64.b64decode(l.strip())) for l in lines]
+        cracker.crack([Message(l) for l in lines])
+    
+    # get final key and save it
+    cracker.save_keys("keys.txt")
+    key = cracker.merge_keys()
+    with open("key.txt", "w") as file:
+        json.dump(key.values, file)
+
+    print("- - - - - - - - - - - - - - - - - - - - - - - - -")
+    print("FINAL KEY: {}".format(key.values))
+    print("- - - - - - - - - - - - - - - - - - - - - - - - -")
 
 if __name__ == "__main__":
     main()
